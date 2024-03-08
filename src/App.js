fe16d63142608c56013bdc3e4dc143f76ab0a796
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2024-03-08 23:30:12 +0530

    full

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React, {useState} from 'react'
-import { useSpeechSynthesis } from 'react-speech-kit';
+import React, { useState } from "react";
+import { useSpeechSynthesis } from "react-speech-kit";
 
- function App() {
-  const [text,setText] = useState('');
-  const {speak} = useSpeechSynthesis();
+function App() {
+  const [text, setText] = useState("");
+  const { speak } = useSpeechSynthesis();
 
   const handleOnClick = () => {
-    speak({text:text})
-  }
+    speak({ text: text });
+  };
 
   return (
-  <div>
-
-    <h1>Text to Speech Converter in React</h1>
-    <textarea className="textAreaStyle" onChange={(e)=>{setText(e.target.value)}}></textarea>
-    <button className="buttonStyle" onClick={()=>{handleOnClick()}}>Listen</button>
-  </div>
-      );
+    <div>
+      <h1>Text to Speech Converter in React</h1>
+      <textarea
+        className="textAreaStyle"
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      ></textarea>
+      <button
+        className="buttonStyle"
+        onClick={() => {
+          handleOnClick();
+        }}
+      >
+        Listen
+      </button>
+    </div>
+  );
 }
 
 export default App;
- 
